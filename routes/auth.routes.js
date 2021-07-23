a5212f265a70ba0faebb3f9575583bb8a59bb30c
elyzx <ellenchildren@icloud.com>
2021-07-23 15:46:04 +0200

    add /user route for checking login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -116,3 +116,23 @@ router.post('/logout', (req, res) => {
 });
 
 module.exports = router;
+
+// middleware to check if user is loggedin
+const isLoggedIn = (req, res, next) => {  
+  if (req.session.loggedInUser) {
+      //calls whatever is to be executed after the isLoggedIn function is over
+      next()
+  }
+  else {
+      res.status(401).json({
+          message: 'Unauthorised user',
+          code: 401,
+      })
+  };
+};
+
+// THIS IS A PROTECTED ROUTE
+// will handle all get requests to http:localhost:5005/api/user
+router.get("/user", isLoggedIn, (req, res, next) => {
+    res.status(200).json(req.session.loggedInUser);
+});
