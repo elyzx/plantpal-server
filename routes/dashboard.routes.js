951ec08dbb3d76ea237256a84e0f542ed091cb19
Humberto Angelucci <angeluccirincon@gmail.com>
2021-07-26 12:24:05 +0200

    dashboard

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -1,4 +1,5 @@
-// GET /dashboard
+const router = require('express').Router();
+const PlantModel = require('../models/Plant.model')
 
 const isLoggedIn = (req, res, next) => {  
     if (req.session.loggedInUser) {
@@ -12,3 +13,21 @@ const isLoggedIn = (req, res, next) => {
         })
     };
   };
+
+// GET /dashboard -- show dashboard 
+// will handle all GET requests to http:localhost:5005/api/dashboard
+  router.get('/dashboard', isLoggedIn, (req, res, next) => {
+    console.log(req.cookie)
+    PlantModel.find()
+        .then((plants) => {
+            res.status(200).json(plants)
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: 'Something went wrong',
+                message: err
+           })
+        });
+});
+
+module.exports = router;
