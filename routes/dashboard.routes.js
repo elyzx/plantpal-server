328a681114b44140d7d5d0ffcb21d26005f17d8b
elyzx <ellenchildren@icloud.com>
2021-07-27 19:29:57 +0200

    installed axios locally and uncommented

--- routes/dashboard.routes.js
+++ routes/dashboard.routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const PlantModel = require('../models/Plant.model')
-// const axios =  require('axios');
+const axios =  require('axios');
 
 const isLoggedIn = (req, res, next) => {  
     if (req.session.loggedInUser) {
@@ -32,15 +32,15 @@ const isLoggedIn = (req, res, next) => {
 });
 
 
-// router.get('/dashboard/test'),isLoggedIn, (req, res, next) => {
-//     axios.get(`http://api.weatherbit.io/v2.0/current?&postal_code=27601&country=US&key=40b97bfea4d145428c756bc5caf74cbb`)
-//     .then( (response) => {
-//         const weatherData = response.data[0]
-//         console.log('weather response :', weatherData)
-//     })
-//     .catch((err) => {
-//       console.log('weather error')
-//        }) 
-// };
+router.get('/dashboard/test'),isLoggedIn, (req, res, next) => {
+    axios.get(`http://api.weatherbit.io/v2.0/current?&postal_code=27601&country=US&key=40b97bfea4d145428c756bc5caf74cbb`)
+    .then( (response) => {
+        const weatherData = response.data[0]
+        console.log('weather response :', weatherData)
+    })
+    .catch((err) => {
+      console.log('weather error')
+       }) 
+};
 
 module.exports = router;
