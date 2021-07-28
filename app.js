20c6dd5ac5e0d9b45daa272552b13e3f250c59e0
elyzx <ellenchildren@icloud.com>
2021-07-28 10:34:39 +0200

    testing

--- app.js
+++ app.js
@@ -30,6 +30,9 @@ app.use(session({
     })
 }));
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 // 👇 Start handling routes here
 // Contrary to the views version, all routes are controlled from the routes/index.js
 const allRoutes = require("./routes");
@@ -52,6 +55,11 @@ app.use("/api", fileUploadRoutes);
 
 const dashboardsRoutes = require('./routes/dashboard.routes')
 app.use("/api", dashboardsRoutes);
+
+app.use((req, res, next) => {
+	// If no routes match, send them the React HTML.
+	res.sendFile(__dirname + "/public/index.html");
+});
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
